4b92d7a8432b2965116b790d15ec99ae6ebb7310
Вадим Руднєв <vadikrudik00@gmail.com>
2026-02-26 06:35:10 +0200

    change task and filemanager

--- src/main/java/com/example/todo/model/Task.java
+++ src/main/java/com/example/todo/model/Task.java
@@ -30,11 +30,4 @@ public class Task
     public int getId() {
         return id;
     }
-
-    public Status getStatus() {
-        return status;
-    }
-
-    public String getTitle() {
-        return title;}
 }
--- src/main/java/com/example/todo/util/FileManager.java
+++ src/main/java/com/example/todo/util/FileManager.java
@@ -1,11 +1,23 @@
 package main.java.com.example.todo.util;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class FileManager
 {
     public static Path getPath(String nameFile)
     {
-        return Path.of("data/" + nameFile + ".txt");
+        Path folder = Path.of("data");
+
+        if(!Files.exists(folder))
+            try {
+                Files.createDirectories(folder);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+        return folder.resolve(nameFile + ".txt");
     }
+
 }
